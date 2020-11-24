22b4f5c221165f0748e322983e944ecea0ce12e0
tiabeanie <sydney.jane.hunter@gmail.com>
2020-11-24 15:38:32 -0700

    added users controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+    def new 
+        user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+            render json: user, except: [:created_at, :updated_at]
+        else
+            render json: {message: "signup incomplete"}
+        end
+    end
+
+    def show
+        user = User.find_by(id: params[:id])
+            if user
+            render json: user, except: [:created_at, :updated_at]
+        else
+            render json: {message: "User not found."}
+        end
+    end
+
+private
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
