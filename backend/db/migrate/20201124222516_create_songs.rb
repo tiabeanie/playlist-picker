d8f7e877a2a7465146fb987e60cf04731687f588
tiabeanie <sydney.jane.hunter@gmail.com>
2020-11-25 10:26:18 -0700

    edited database

--- backend/db/migrate/20201124222516_create_songs.rb
+++ backend/db/migrate/20201124222516_create_songs.rb
@@ -4,7 +4,6 @@ class CreateSongs < ActiveRecord::Migration[6.0]
       t.string :title
       t.string :artist
       t.string :genre
-      t.references :playlist, null: false, foreign_key: true
 
       t.timestamps
     end
--- backend/db/migrate/20201124222615_create_playlists.rb
+++ backend/db/migrate/20201124222615_create_playlists.rb
@@ -1,8 +1,8 @@
 class CreatePlaylists < ActiveRecord::Migration[6.0]
   def change
     create_table :playlists do |t|
-      t.string :title
-      t.integer :owner
+      t.integer :user_id
+      t.integer :song_id
 
       t.timestamps
     end
